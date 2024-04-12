751399c36b0d67df885a575ece95619efda0d693
EstebanTamayo <es.tamayo@duocuc.cl>
2024-04-12 16:06:30 -0400

    Agregando vista Inicio Menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
+import InicioMenu from './inicioMenu/components/InicioMenu'
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <InicioMenu />
+    </>
   )
 }
 
---
+++ src/inicioMenu/components/InicioMenu.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const InicioMenu = () => {
+  return (
+    <div className='text-center'>
+        <h1 className='underline-offset-1'>Menu</h1>
+        <h2>Inicio</h2>
+    </div>
+  )
+}
+
+export default InicioMenu
